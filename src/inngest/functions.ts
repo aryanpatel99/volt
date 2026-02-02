af2f485fdfecdf11f1b99edadf68b50191cc239e
Aryan Patel <aryanpatel6215@gmail.com>
2026-02-03 00:04:54 +0530

    added background jobs

---
+++ src/inngest/functions.ts
@@ -0,0 +1,15 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world " },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    // downlaod step
+    await step.sleep("wait-a-moment", "30s");
+    // transcritp step
+    await step.sleep("wait-a-moment", "10s");
+    // summary step
+    await step.sleep("wait-a-moment", "5s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
